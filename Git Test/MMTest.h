cdd509511a89011987b5c7c4e73dd76d0bff783d
Patrice Warrick <patricewarrick@Patrices-iMac.local>
2014-05-22 12:51:08 -0400

    added a test class

---
+++ Git Test/MMTest.h
@@ -0,0 +1,13 @@
+//
+//  MMTest.h
+//  Git Test
+//
+//  Created by Patrice Warrick on 5/22/14.
+//  Copyright (c) 2014 Meepmom. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MMTest : NSObject
+
+@end
---
+++ Git Test/MMTest.m
@@ -0,0 +1,13 @@
+//
+//  MMTest.m
+//  Git Test
+//
+//  Created by Patrice Warrick on 5/22/14.
+//  Copyright (c) 2014 Meepmom. All rights reserved.
+//
+
+#import "MMTest.h"
+
+@implementation MMTest
+
+@end
